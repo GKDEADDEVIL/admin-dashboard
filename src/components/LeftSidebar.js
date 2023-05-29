5797966997671aca8b2e435e4726da4f050b32af
GKDAREDEVIL <kalyangopalam@gmail.com>
2023-05-29 08:11:45 +0530

    initial commit

--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -5,8 +5,8 @@ import { logo, leftbar, subs, subsr } from '../assets';
 
 const LeftSidebar = () => {
     return (
-        <div className="flex flex-col items-center justify-between pl-4 pr-2 h-max bg-gray-200 ">
-            <div className="flex items-center mb-20 pt-4">
+        <div className="flex flex-col items-center justify-between pl-4 pr-2 h-max bg-gray-200 rounded-md">
+            <div className="flex items-center mb-20 pt-4 cursor-pointer">
                 <img
                     src={logo}
                     alt="Skilify Admin"
@@ -15,7 +15,7 @@ const LeftSidebar = () => {
                 <h1 className="text-lg font-bold">Skilify Admin</h1>
             </div>
             <nav className="flex flex-col space-y-2 pb-6"> 
-                <div className="nav-link transition duration-300 hover:bg-gray-800 p-4 rounded-3xl w-60 flex items-center justify-between pl-8 pr-8">
+                <div className="nav-link transition duration-1000 hover:bg-gray-800 p-4 rounded-3xl w-60 flex items-center justify-between pl-8 pr-8">
                     <span>
                         <MdDashboard className="mr-2 inline-block" />
                         Dashboard
@@ -58,17 +58,21 @@ const LeftSidebar = () => {
                     <MdKeyboardArrowRight className="inline-block" />
                 </div>
             </nav>
-            <div className="my-8">
-                <img
-                    src={leftbar}
-                    alt="Left Bar Image"
-                    className="w-full"
-                />
+            <div className="bg-slate-300 rounded-2xl mb-6 mt-8 w-full">
+                <div className="my-8 flex justify-center">
+                    <img
+                        src={leftbar}
+                        alt="Left Bar Image"
+                        className="w-auto h-32"
+                    />
+                </div>
+                <div className="flex justify-center ml-2 mr-2">
+                    <button className="mt-4 mb-6 bg-white hover:bg-red-600  text-red-600 hover:text-white duration-700  font-bold py-2 px-4 rounded-3xl w-64 flex justify-around items-center ">
+                        <img src={subs} alt="" /> 
+                        Get Subscription 
+                    </button>
+                </div>
             </div>
-            <button className="mt-4 mb-6 bg-white hover:bg-red-600  text-red-600 hover:text-white duration-700  font-bold py-2 px-4 rounded-3xl w-64 flex justify-around items-center ">
-                <img src={subs} alt="" /> 
-                Get Subscription 
-            </button>
         </div>
     );
 };
